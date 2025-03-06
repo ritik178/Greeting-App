69ece065cbf160950ed7d1949906a8b4d6f60d7f
ritik <ritikstm16@gmail.com>
2025-03-06 20:02:07 +0530

    deployed commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,31 +8,44 @@ function App() {
 
   // State to store the greeting message
   const [message, setMessage] = useState('');
+
+  // State to store the error Message
+  const [errorMessage, setErrorMessage] = useState('');
   
   // Function to fetch the greeting message from the backend
   const fetchGreeting = async() => {
+    // If the message or errorMessage is already set then reset it
+    setErrorMessage('');
+    setMessage('');
     // Check if the name is empty or not
     if(!name.trim()){
-      setMessage('Name is required.');
+      setErrorMessage('Name is required.');
       return;
     }
     try{
         // Fetch the greeting message from the backend API
-        const response = await fetch(`http://localhost:5000/api/greet?name=${name}`);
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/greet?name=${name}`);
         const data = await response.json();
         // Set the greeting message in the state
-        setMessage(data.message || data.error);
+        if(data.message){
+          setMessage(data.message);
+        }
+        if(data.error){
+          setErrorMessage(data.error);
+        }
     }catch(error){
       // Handle any errors that occur during the fetch
-      setMessage('Error fetching greeting.');
+      setErrorMessage('Error fetching greeting.');
+      console.log("error:",error);
     }
   };
   
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
       {/* Display the greeting message if it exists */}
       {message && <p className="mt-4 text-lg text-green-600">{message}</p>}
+      {errorMessage && <p className="mt-4 text-lg text-red-600">{errorMessage}</p>}
       <h1 className="text-2xl font-bold mb-4">Greeting App</h1>
       {/* Input field for the user to enter their name */}
       <input
