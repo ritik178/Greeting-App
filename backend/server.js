8c4fadd3d5a936caf5a9850070c89fd60e6ff976
ritik <ritikstm16@gmail.com>
2025-03-06 17:29:36 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,21 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors()); // Enable CORS for frontend requests
 
+
+// Define a route to greet the user
 app.get('/api/greet', ( req, res) => {
+    // Get the name from the query parameters(send by the frontend)
     const name = req.query.name;
+    // Check if the name is empty or not
     if(!name){
+        // Send an error response if the name is empty with a status code of 400
         return res.status(400).json({ error: "Name is required."});
     }
+    // Send a greeting message if the name is not empty
     res.json({ message: `Hello, ${name}! Welcome to Younglabs.`});
 });
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,28 @@ import { useState } from "react";
 
 
 function App() {
+
+  // State to store the user's name
   const [name, setName] = useState('');
+
+  // State to store the greeting message
   const [message, setMessage] = useState('');
   
+  // Function to fetch the greeting message from the backend
   const fetchGreeting = async() => {
+    // Check if the name is empty or not
     if(!name.trim()){
       setMessage('Name is required.');
       return;
     }
     try{
+        // Fetch the greeting message from the backend API
         const response = await fetch(`http://localhost:5000/api/greet?name=${name}`);
         const data = await response.json();
+        // Set the greeting message in the state
         setMessage(data.message || data.error);
     }catch(error){
+      // Handle any errors that occur during the fetch
       setMessage('Error fetching greeting.');
     }
   };
@@ -22,8 +31,10 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      {/* Display the greeting message if it exists */}
       {message && <p className="mt-4 text-lg text-green-600">{message}</p>}
       <h1 className="text-2xl font-bold mb-4">Greeting App</h1>
+      {/* Input field for the user to enter their name */}
       <input
         type="text"
         placeholder="Enter your name"
@@ -31,6 +42,8 @@ function App() {
         onChange={(e) => setName(e.target.value)}
         className="border p-2 mb-4 rounded"
         />
+
+      {/* Button to fetch the greeting message */}
       <button
         onClick={fetchGreeting}
         className="bg-blue-500 text-white px-4 py-2 rounded "
